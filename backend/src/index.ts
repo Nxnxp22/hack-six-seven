b241ab9bc0f7181adac5550a6a8c6553788da1c0
wiininjulyyy <thanarat.chai@mail.kmutt.ac.th>
2026-05-31 07:08:55 +0700

    fixing gamePage

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 import challengesRouter from "./modules/comm-failure/routes/challenge.route";
 
 const app  = express();
@@ -8,8 +9,9 @@ const PORT = process.env.PORT ?? 3000;
 
 app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
-
+app.use(morgan('dev'));
 app.use("/api/challenges", challengesRouter);
+
 
 app.listen(PORT, () => {
   console.log(`API running on http://localhost:${PORT}`);
